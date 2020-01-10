6649fb8096f7a1e101fecb68d0c05ace5d28f8e9
Иван Гагаринов <i.gagarinov@bright-soft.ru>
2020-01-10 09:09:57 +0500

    refactorint isPrime

--- src/games/IsPrime.php
+++ src/games/IsPrime.php
@@ -6,22 +6,14 @@ use function BrainGames\Cli\run;
 
 const GAME_RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-function isPrime(int $question): bool
+function isPrime(int $number): bool
 {
-    $isPrime = function (int $n, int $i) use (&$isPrime): bool {
-        if ($n < 2) {
+    for ($x = 2; $x <= sqrt($number); $x++) {
+        if ($number % $x === 0) {
             return false;
-        } elseif ($n == 2) {
-            return true;
-        } elseif ($n % $i == 0) {
-            return false;
-        } elseif ($i < $n / 2) {
-            return $isPrime($n, $i + 1);
-        } else {
-            return true;
         }
-    };
-    return $isPrime($question, 2);
+    }
+    return true;
 }
 
 function game()
@@ -29,10 +21,9 @@ function game()
     $getQuestionAndAnswer = function () {
         $question = rand(1, 100);
         $answer = isPrime($question) ? 'yes' : 'no';
-        $question = strval($question);
         return [
             'answer' => $answer,
-            'question' => $question,
+            'question' => strval($question),
         ];
     };
     run(GAME_RULE, $getQuestionAndAnswer);
